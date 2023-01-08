bca80237f6d493984b735af6d28b931b82aad0a2
imjayapple <imjayapple@gmail.com>
2023-01-07 19:19:43 -0800

    Add switch statement for all 8 (0-7) eight ball answers

--- app.js
+++ app.js
@@ -9,3 +9,33 @@ let eightBall = '';
 
 //Our eightBall will have 8 total questions so generate a random whole number between 0 (inclusive) and 9 (exclusive)
 let randomNumber = Math.floor(Math.random() * 8);
+
+//Switch statement that takes the randomNumber input and outputs the appropriate string/eightBall assignment
+switch (randomNumber) {
+  case (0):
+    eightBall = 'It is certain';
+     break;
+  case (1):
+    eightBall = 'It is decidedly so'
+     break;
+  case (2):
+    eightBall = 'Reply hazy try again'
+     break;
+  case (3):
+    eightBall = 'Cannot predict now'
+     break;
+  case (4):
+    eightBall = 'Do not count on it'
+     break;
+  case (5):
+    eightBall = 'My sources say no'
+     break;
+  case (6):
+    eightBall = 'Outlook not so good'
+     break;
+  case (7):
+    eightBall = 'Signs point to yes'
+     break;
+  Default:
+    console.log('Invalid entry');
+}
